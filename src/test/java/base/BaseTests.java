98fd3a0f3bf5b2b7cac4405f4ec03ab7f1be5498
LogicuL <47316331+LogicuL@users.noreply.github.com>
2020-10-09 15:30:53 +0300

    added base test

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,6 +1,26 @@
 package base;
 
+import com.paulhammant.ngwebdriver.NgWebDriver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+
 public class BaseTests
 {
-    private WebDriver
+    private WebDriver driver;
+    private NgWebDriver ngWebDriver;
+
+    @BeforeClass
+    public void setup()
+    {
+        // set the property for the chrome driver and point to the location
+
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        JavascriptExecutor jsDriver = (JavascriptExecutor) driver;
+        ngWebDriver = new NgWebDriver(jsDriver);
+
+    }
 }
